84d851177063e283abdb611ec16e5ba303a49185
junhong91 <junhong199@gmail.com>
2021-04-23 14:05:17 +0900

    Fix mis-spell  from

--- ToDo/server.js
+++ ToDo/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({enxtended : true}));
+app.use(bodyParser.urlencoded({extended : true}));
 app.set('view engine', 'ejs');
 
 app.use('/public', express.static('public'));
